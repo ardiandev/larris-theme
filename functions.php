cfdfe25294560bc586f43f004e3fcbbbdd32c93c
ardianysp <ardianysp18@gmail.com>
2025-03-18 00:20:25 +0700

    Add test pattern 3

--- functions.php
+++ functions.php
@@ -1,22 +1,19 @@
 <?php
-// Register block patterns only in the admin panel
-add_action('init', function() {
-    if (!is_admin()) {
-        return; // Prevent execution on the front-end
-    }
 
-    // Clear cached patterns
+add_action('admin_init', function() {
+    // Clear the cached patterns
     delete_transient('wp_block_patterns');
 
+    // Path to the patterns directory
     $patterns_dir = get_template_directory() . '/patterns/';
 
+    // Check if the directory exists
     if (is_dir($patterns_dir)) {
         foreach (glob($patterns_dir . '*.php') as $file) {
-            $pattern_data = include $file;
-
-            if (is_array($pattern_data) && isset($pattern_data['title'], $pattern_data['content'])) {
-                register_block_pattern('larris-theme/' . basename($file, '.php'), $pattern_data);
-            }
+            register_block_pattern(
+                'larris-theme/' . basename($file, '.php'),
+                require $file
+            );
         }
     }
 });
