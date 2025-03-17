2993e51538572329f26e0acb0198a66ba646f924
ardianysp <ardianysp18@gmail.com>
2025-03-18 01:49:01 +0700

    Add test pattern 3

--- patterns/test5.php
+++ patterns/test5.php
@@ -5,4 +5,8 @@
  * Categories: test
  */
 ?>
-<p>Test Pattern 5</p>
+<!-- wp:group {"align":"full","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull"><!-- wp:heading {"backgroundColor":"vivid-cyan-blue","fontSize":"large"} -->
+<h2 class="wp-block-heading has-vivid-cyan-blue-background-color has-background has-large-font-size">Test Pattern</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
