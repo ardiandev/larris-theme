2b5a6c4283b1328fa48a1fbe74923a6206c31a81
ardianysp <ardianysp18@gmail.com>
2025-03-18 00:13:26 +0700

    fix fn2

--- functions.php
+++ functions.php
@@ -1,19 +1,18 @@
 <?php
-
+// Automatically scan and register block patterns in the 'patterns' directory when in admin panel
 add_action('admin_init', function() {
-    // Clear the cached patterns
+    // Clear cached patterns
     delete_transient('wp_block_patterns');
 
-    // Path to the patterns directory
     $patterns_dir = get_template_directory() . '/patterns/';
 
-    // Check if the directory exists
     if (is_dir($patterns_dir)) {
         foreach (glob($patterns_dir . '*.php') as $file) {
-            register_block_pattern(
-                'larris-theme/' . basename($file, '.php'),
-                require $file
-            );
+            $pattern_data = require $file; // Load pattern array from file
+
+            if (is_array($pattern_data) && isset($pattern_data['title'], $pattern_data['content'])) {
+                register_block_pattern('larris-theme/' . basename($file, '.php'), $pattern_data);
+            }
         }
     }
 });
