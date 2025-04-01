2253608df3a92961564e84d2af8fd6699480fde8
Ardian Pradana <ardianysp18@gmail.com>
2025-04-02 03:13:12 +0700

    Edit contact-us pattern

--- patterns/contact-us.php
+++ patterns/contact-us.php
@@ -11,7 +11,7 @@
 
 <!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}},"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}}},"backgroundColor":"secondary","textColor":"primary","layout":{"type":"constrained","contentSize":"800px"}} -->
 <div class="wp-block-group alignfull has-primary-color has-secondary-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--20)"><!-- wp:heading {"textAlign":"center"} -->
-<h2 class="wp-block-heading has-text-align-center">Contact1</h2>
+<h2 class="wp-block-heading has-text-align-center">CONTACT</h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"center"} -->
