3aa3c30938f6e6957385c7ccebe94532c8d72bb6
Ardian Pradana <ardianysp18@gmail.com>
2025-04-02 03:17:44 +0700

    Edit hero pattern

--- patterns/hero.php
+++ patterns/hero.php
@@ -7,18 +7,19 @@
  */
 
 ?>
-<!-- wp:cover {"overlayColor":"primary","isUserOverlayColor":true,"minHeight":90,"minHeightUnit":"vh","align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|30","padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}},"elements":{"heading":{"color":{"text":"var:preset|color|secondary"}},"link":{"color":{"text":"var:preset|color|secondary"}}}},"textColor":"secondary","layout":{"type":"constrained","contentSize":"1000px"}} -->
+
+<!-- wp:cover {"overlayColor":"primary","isUserOverlayColor":true,"minHeight":90,"minHeightUnit":"vh","metadata":{"categories":["banner"],"patternName":"larris-theme/hero","name":"Hero Larris Theme"},"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|30","padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}},"elements":{"heading":{"color":{"text":"var:preset|color|secondary"}},"link":{"color":{"text":"var:preset|color|secondary"}}}},"textColor":"secondary","layout":{"type":"constrained","contentSize":"1000px"}} -->
 <div class="wp-block-cover alignfull has-secondary-color has-text-color has-link-color" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);min-height:90vh"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1,"fontSize":"xx-large"} -->
 <h1 class="wp-block-heading has-text-align-center has-xx-large-font-size">Hi, I’m Ardian Pradana.</h1>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":1.6}}} -->
-<p class="has-text-align-center" style="line-height:1.6">I develop powerful, custom WordPress solutions. From themes to plugins—built for performance and scalability.</p>
+<!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":1.6,"fontSize":"1.25rem"}}} -->
+<p class="has-text-align-center" style="font-size:1.25rem;line-height:1.6">I develop powerful, custom WordPress solutions. From themes to plugins—built for performance and scalability.</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"tertiary","textColor":"primary","style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}},"spacing":{"padding":{"left":"var:preset|spacing|20","right":"var:preset|spacing|20","top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}},"border":{"radius":"5px"}},"fontSize":"medium"} -->
-<div class="wp-block-button has-custom-font-size has-medium-font-size"><a class="wp-block-button__link has-primary-color has-tertiary-background-color has-text-color has-background has-link-color wp-element-button" style="border-radius:5px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)">PROJECTS</a></div>
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"tertiary","textColor":"primary","style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}},"spacing":{"padding":{"left":"var:preset|spacing|50","right":"var:preset|spacing|50","top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}},"border":{"radius":"5px"},"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"medium"} -->
+<div class="wp-block-button has-custom-font-size has-medium-font-size" style="font-style:normal;font-weight:600"><a class="wp-block-button__link has-primary-color has-tertiary-background-color has-text-color has-background has-link-color wp-element-button" style="border-radius:5px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--50)">PROJECTS</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div></div>
 <!-- /wp:cover -->
